d8d73768479b706f2aa47a50bdc2682d7ae0affb
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2017-02-23 10:41:25 -0300

    Some examples for usage of putPixel, drawLine and drawTriangle functions added

--- main.cpp
+++ main.cpp
@@ -6,9 +6,19 @@ void MyGlDraw(void)
 	//*************************************************************************
 	// Chame aqui as funções do mygl.h
 	//*************************************************************************
-
-
-
+	//putPixel(-254, 254, colorGreen);
+	// drawLine(0,0, colorRed, 200, 100, colorGreen); //1 octante
+	// drawLine(0,0, colorRed, 100, 200, colorGreen); //2 octante
+	// drawLine(0,0, colorRed, -100, 200, colorGreen); //3 octante
+	//drawLine(0,0, colorRed, -200, 100, colorGreen); //4 octante
+	//drawLine(0,0, colorRed, -200, -100, colorGreen); //5 octante
+	// drawLine(0,0, colorRed, -100, -200, colorGreen); //6 octante
+	//drawLine(0,0, colorRed, 100, -200, colorGreen); //7 octante
+	//drawLine(0,0, colorRed, 200, -100, colorGreen); //8 octante
+	//drawLine(-100, 0, colorRed, -100, 90, colorGreen);
+	//drawLine(-100, 0, colorRed, 100, 0, colorBlue);
+	//drawLine(-100, 90, colorGreen, 100, 0, colorBlue);
+	drawTriangle(-100, 0, colorRed, -100, 90, colorGreen, 100, 0, colorBlue);
 }
 
 //-----------------------------------------------------------------------------
